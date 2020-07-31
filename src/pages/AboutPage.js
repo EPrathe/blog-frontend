2dc66d0d677386e125c749388caa4845ce53a35c
Evan Prather <pratherevan@gmail.com>
2020-07-31 17:07:22 -0500

    another one

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,16 +4,16 @@ const AboutPage = () => (
     <div className="fade-in">
     <h1>About me</h1>
     <p>
-        Hello! I graduated College with a degree in Computer Science along with minors in Bussiness and Mathmatics in 2017.
+        Hello! I'm Evan Prather. I graduated College with a degree in Computer Science along with minors in Business and Mathematics in 2017.
         </p>
         <p>
-        I spent one year working as a full stack developer at Unisys where I created software that allowed users 
+        I spent one year working as a full stack developer at Unisys, there I created software that allowed users 
         to apply for loans from the USDA.
         </p>
         <p>
         Next I worked for two years as a front end devloper at Martiz Motivation. 
-        At Maritz I worked on webbased applications that companies
-        the ability to create rewareds programs. 
+        At Maritz I worked on web based applications that gave companies
+        the ability to create rewards programs. 
     </p>
     <p>I enjoy learning new technologies and working on teams where colaboration are encouraged.</p>  
     <p>
